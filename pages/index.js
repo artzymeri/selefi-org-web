803edbca61d110186ab416380cfb274ae15b3d53
Art Z <artz.@Arts-MacBook-Pro.local>
2025-08-03 16:04:40 +0200

    Dua in the beggining added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import LayoutHOC from '@/components/LayoutHOC/LayoutHOC';
 import HeroSection from '@/components/HomePage/HeroSection';
+import Baslamah from '@/components/HomePage/Basmalah';
 
 export default function Home() {
   return (
     <LayoutHOC title="Ballina">
+      <Baslamah />
       <HeroSection srcImage="/ballina.png" altText="Ballina" />
     </LayoutHOC>
   );
---
+++ src/components/HomePage/Basmalah.jsx
@@ -0,0 +1,13 @@
+const Baslamah = () => {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <img
+        src="/baslamah.png"
+        className="w-1/6 object-contain mb-6"
+        alt="Baslamah"
+      />
+    </div>
+  );
+};
+
+export default Baslamah;
