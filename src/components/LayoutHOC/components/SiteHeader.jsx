36cf89bad938b514754f045af2adaee48e98b9f8
Art Z <artz.@Arts-MacBook-Pro.local>
2025-08-03 16:14:06 +0200

    Placed search bar on header

--- src/components/LayoutHOC/components/SiteHeader.jsx
+++ src/components/LayoutHOC/components/SiteHeader.jsx
@@ -8,18 +8,30 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import LinkButtons from '@/components/LayoutHOC/components/site-header-components/LinkButtons';
+import { Input } from '@/components/ui/input';
+import { SearchIcon } from 'lucide-react';
 
 export function SiteHeader({ title = '' }) {
   return (
     <header className="bg-white/30 backdrop-blur-sm sticky top-0 z-50 flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6 h-full">
         <SidebarTrigger className="-ml-1" />
         <Separator
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
         <h1 className="text-base font-medium">{title}</h1>
-        <LinkButtons />
+        <div className="flex items-center gap-2 ml-auto">
+          <div className="relative w-full">
+            <SearchIcon className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+            <Input type="text" placeholder="Kërko" className="pl-10 h-min" />
+          </div>
+          <Separator
+            orientation="vertical"
+            className="mx-2 data-[orientation=vertical]:h-4"
+          />
+          <LinkButtons />
+        </div>
       </div>
     </header>
   );
