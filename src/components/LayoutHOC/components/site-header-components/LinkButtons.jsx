8b26f75aa9a21052c389eb661028ed527f6436f4
Art Z <artz.@Arts-MacBook-Pro.local>
2025-08-07 12:30:11 +0200

    Added IG and Tiktok to header

--- src/components/LayoutHOC/components/site-header-components/LinkButtons.jsx
+++ src/components/LayoutHOC/components/site-header-components/LinkButtons.jsx
@@ -1,8 +1,11 @@
 import LinkButton from '@/components/LayoutHOC/components/site-header-components/LinkButton';
 import {
-  IconBrandFacebookFilled,
+  IconBrandFacebook,
+  IconBrandInstagram,
   IconBrandTelegram,
-  IconBrandYoutubeFilled,
+  IconBrandTiktok,
+  IconBrandTiktokFilled,
+  IconBrandYoutube,
 } from '@tabler/icons-react';
 
 const LinkButtons = () => {
@@ -15,13 +18,23 @@ const LinkButtons = () => {
       />
       <LinkButton
         link="https://youtube.com/@AudioSelefi"
-        icon={<IconBrandYoutubeFilled />}
+        icon={<IconBrandYoutube />}
         tooltipContent="Linku i Kanalit Youtube"
       />
       <LinkButton
         link="https://www.facebook.com/Selefi.org.official/"
-        icon={<IconBrandFacebookFilled />}
+        icon={<IconBrandFacebook />}
         tooltipContent="Linku i Faqes Facebook"
+      />
+      <LinkButton
+        link="https://instagram.com/selefi_org?igshid=YmMyMTA2M2Y="
+        icon={<IconBrandInstagram />}
+        tooltipContent="Linku i Faqes Instagram"
+      />
+      <LinkButton
+        link="https://www.tiktok.com/@selefi.org?_t=8npSoodsRyH&_r=1"
+        icon={<IconBrandTiktok />}
+        tooltipContent="Linku i Faqes Tiktok"
       />
     </div>
   );
